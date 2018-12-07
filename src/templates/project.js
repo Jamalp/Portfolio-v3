c74b0ac2446125e6c3be2d610a9624558d07d7c2
Jamal Powell <jamalkpowell@gmail.com>
2018-12-07 12:43:47 -0800

    update with renamed files

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import styled from "@emotion/styled";
 import { vars } from "../utils/emotionVars";
